a8f43ee27db48d298c4163b35015a58ba2d85e00
joao <joao.victor@topsapp.com.br>
2021-11-01 17:32:01 -0400

    criado primeiro componente

---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+
+    background-color: ${({theme})=> theme.colors.background};
+`
+
+export const Title = styled.Text`
+    color: ${({theme})=> theme.colors.title}
+`
