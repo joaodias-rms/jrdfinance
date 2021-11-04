ed885cff13bdb5d32dc83f4e4d42348346981c1f
joao <jvd.ramos@hotmail.com>
2021-11-04 13:23:22 -0400

    adicionado carregamento de fontes

--- App.tsx
+++ App.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components/native';
+import React from "react";
+import { ThemeProvider } from "styled-components/native";
 
-import { Dashboard } from './src/screens/Dashboard';
+import { Dashboard } from "./src/screens/Dashboard";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
-import theme from './src/global/Styles/theme';
+import theme from "./src/global/Styles/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
-     <Dashboard/>
+      <Dashboard />
     </ThemeProvider>
   );
 }
-
