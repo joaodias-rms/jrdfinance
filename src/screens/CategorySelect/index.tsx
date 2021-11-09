fbb00d01ee5eaa131587b2d61c0e63d79163235b
joao <jvd.ramos@hotmail.com>
2021-11-09 11:24:35 -0400

    criado componente de listagem de categorias

---
+++ src/screens/CategorySelect/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { FlatList } from "react-native";
+
+import {
+  Container,
+  Header,
+  Title,
+  Category,
+  Icon,
+  Name,
+  Separator,
+  Footer,
+} from "./styles";
+
+import { Button } from "../../components/Forms/Button";
+
+import { categories } from "../../utils/categories";
+
+interface CategoryProps {
+  key: string;
+  name: string;
+}
+
+interface Props {
+  category: CategoryProps;
+  setCategory: (category: CategoryProps) => void;
+  closeSelectCategory: () => void;
+}
+
+export function CategorySelect({
+  category,
+  setCategory,
+  closeSelectCategory,
+}: Props) {
+  return (
+    <Container>
+      <Header>
+        <Title>Categoria</Title>
+      </Header>
+
+      <FlatList
+        data={categories}
+        style={{ flex: 1, width: "100%" }}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <Category>
+            <Icon name={item.icon} />
+            <Name>{item.name}</Name>
+          </Category>
+        )}
+        ItemSeparatorComponent={()=><Separator/>}
+      />
+      <Footer>
+          <Button title="Selecionar" onPress={closeSelectCategory}/>
+      </Footer>
+    </Container>
+  );
+}
