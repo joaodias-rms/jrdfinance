1e79a141d979a4473d0482341488db707f59cb80
joao <jvd.ramos@hotmail.com>
2021-11-09 17:36:47 -0400

    criado rotas da aplicação

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { Dashboard } from "../screens/Dashboard";
+import { Register } from "../screens/Register";
+import theme from "../global/Styles/theme";
+import { Platform } from "react-native";
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: "beside-icon",
+        tabBarStyle: {
+          height: 88,
+          paddingVertical: Platform.OS === "ios" ? 20 : 0,
+        },
+      }}
+    >
+      <Screen
+        name="Listagem"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              size={size}
+              color={color}
+              name="format-list-bulleted"
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="Cadastrar"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons size={size} color={color} name="attach-money" />
+          ),
+        }}
+      />
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons size={size} color={color} name="pie-chart" />
+          ),
+        }}
+      />
+    </Navigator>
+  );
+}
