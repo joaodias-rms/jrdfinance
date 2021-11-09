90239706bc9d04e4955d05aa573c976a25500343
joao <jvd.ramos@hotmail.com>
2021-11-09 17:36:38 -0400

    adicionado navegação por rotas no app

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { AppRoutes } from "./src/routes/app.routes";
 
 import { Register } from "./src/screens/Register";
 import AppLoading from "expo-app-loading";
@@ -25,7 +27,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
