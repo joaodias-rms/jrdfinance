83a04b857301792d60e502843b9556c780a11be5
joao <jvd.ramos@hotmail.com>
2021-11-09 11:24:18 -0400

    Criado botão pra abrir modal de categorias

---
+++ src/components/Forms/CategorySelectInput/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+import { Container, Category, Icon } from "./styles";
+
+interface Props {
+  title: string;
+  onPress: ()=> void
+}
+
+export function CategorySelectInput({ title, onPress }: Props) {
+  return (
+    <Container onPress={onPress}>
+      <Category>{title}</Category>
+      <Icon name="chevron-down" />
+    </Container>
+  );
+}
