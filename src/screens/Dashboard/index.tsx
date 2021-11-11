70002d035239053fc5e67b6b07f9221f48ffdf8a
joao <jvd.ramos@hotmail.com>
2021-11-11 13:37:26 -0400

    refatorado data das últimas transações

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -18,7 +18,7 @@ import {
   TransactionsList,
   Title,
   LogoutButton,
-  LoadContainer
+  LoadContainer,
 } from "./styles";
 
 import { HighlightCard } from "../../components/HighlightCard";
@@ -35,6 +35,7 @@ export interface TransactionListProps extends TransactionCardProps {
 
 interface HighlightCardProps {
   amount: string;
+  lastTransactions: string;
 }
 interface HighlightCardData {
   entries: HighlightCardProps;
@@ -48,6 +49,26 @@ export function Dashboard() {
   const [highlighCardData, setHighlightCardData] = useState<HighlightCardData>(
     {} as HighlightCardData
   );
+
+  //função pra buscar a data da última transação
+  function getLastData(
+    collection: TransactionListProps[],
+    type: "incoming" | "outcoming"
+  ) {
+    const lastTransactions = new Date(
+      Math.max.apply(
+        Math,
+        collection
+          .filter((transaction) => transaction.type === type)
+          .map((transaction) => new Date(transaction.date).getTime())
+      )
+    );
+
+    return `${lastTransactions.getDate()} de ${lastTransactions.toLocaleString(
+      "pt-BR",
+      { month: "long" }
+    )}`;
+  }
 
   async function loadTransactions() {
     const dataKey = "@jrdfinance:transactions";
@@ -88,6 +109,10 @@ export function Dashboard() {
     );
     setData(transactionsFormatted);
 
+    const lastTransactionEntries = getLastData(transactions, "incoming");
+    const lastTransactionExpenses = getLastData(transactions, "outcoming");
+    const totalInterval = `Movimento de 01 a ${lastTransactionExpenses}`
+
     const total = entriesSum - expensesSum;
 
     setHighlightCardData({
@@ -96,21 +121,24 @@ export function Dashboard() {
           style: "currency",
           currency: "BRL",
         }),
+        lastTransactions: `Última entrada dia ${lastTransactionEntries}`,
       },
       expenses: {
         amount: expensesSum.toLocaleString("pt-BR", {
           style: "currency",
           currency: "BRL",
         }),
+        lastTransactions: `Última saída dia ${lastTransactionExpenses}`,
       },
       total: {
         amount: total.toLocaleString("pt-BR", {
           style: "currency",
           currency: "BRL",
         }),
+        lastTransactions: totalInterval,
       },
     });
-    setIsLoadign(false)
+    setIsLoadign(false);
   }
 
   useEffect(() => {
@@ -127,7 +155,7 @@ export function Dashboard() {
     <Container>
       {isLoadign ? (
         <LoadContainer>
-          <ActivityIndicator color={theme.colors.secondary} size="large"/>
+          <ActivityIndicator color={theme.colors.secondary} size="large" />
         </LoadContainer>
       ) : (
         <>
@@ -153,19 +181,19 @@ export function Dashboard() {
               type="down"
               title="Entradas"
               amount={highlighCardData.entries.amount}
-              lastTransaction="Última entrada em 03 de novembro"
+              lastTransaction={highlighCardData.entries.lastTransactions}
             />
             <HighlightCard
               type="up"
               title="Saídas"
               amount={highlighCardData.expenses.amount}
-              lastTransaction="Última saída em 03 de novembro"
+              lastTransaction={highlighCardData.expenses.lastTransactions}
             />
             <HighlightCard
               type="total"
               title="Total"
               amount={highlighCardData.total.amount}
-              lastTransaction="Movimento de 01 a 06 de novembro"
+              lastTransaction={highlighCardData.total.lastTransactions}
             />
           </HighlightCards>
           <Transactions>
