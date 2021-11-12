1d02329c81b5314e008c3fba7ae49c4d56a842f4
joao <jvd.ramos@hotmail.com>
2021-11-12 14:23:31 -0400

    criado componente de login social

---
+++ src/components/SignInSocialButton/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { RectButtonProps } from "react-native-gesture-handler";
+import { SvgProps } from "react-native-svg";
+
+import { Container, Button, ImageContainer, Text } from "./styles";
+
+interface Props extends RectButtonProps {
+  title: string;
+  svg: React.FC<SvgProps>;
+}
+
+export function SignInSocialButton({ title, svg: Svg, ...rest }: Props) {
+  return (
+    <Container>
+      <Button {...rest}>
+        <ImageContainer>
+          <Svg />
+        </ImageContainer>
+        <Text>{title}</Text>
+      </Button>
+    </Container>
+  );
+}
