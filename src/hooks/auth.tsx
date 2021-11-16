a0520aeee876e0a37ca9a39843183985c1a08b8c
joao <jvd.ramos@hotmail.com>
2021-11-16 11:17:14 -0400

    tipado dados de login com google

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,38 +1,77 @@
-import React,{ createContext, ReactNode, useContext } from "react";
+import React, { createContext, ReactNode, useContext } from "react";
 
+import * as AuthSession from "expo-auth-session";
+import { useState } from "react";
 
-interface AuthProviderProps{
-    children: ReactNode
+interface AuthProviderProps {
+  children: ReactNode;
 }
 
 interface User {
-    id: string;
-    name: string;
-    email: string;
-    photo?: string
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
 }
 
-interface AuthContextData{
-    user: User
+interface AuthContextData {
+  user: User;
+  signInWithGoogle(): Promise<void>;
 }
-const AuthContext = createContext({}as AuthContextData);
 
-function AuthProvider({children}: AuthProviderProps) {
-    const user = {
-        id: '1',
-        name: 'João',
-        email: 'joaodias@unet.br'
+interface AuthorizationResponse {
+  params: {
+    access_token: string;
+  };
+  type: string;
+}
+
+const AuthContext = createContext({} as AuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>({} as User);
+  async function signInWithGoogle() {
+    try {
+      const CLIENT_ID =
+        "878777574638-g910olnofuvi4pn47hflcnoihi8rur22.apps.googleusercontent.com";
+      const REDIRECT_URI = "https://auth.expo.io/@joaodias-rms/jdrfinance";
+      const RESPONSE_TYPE = "token";
+      const SCOPE = encodeURI("profile email");
+
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+      const { type, params } = (await AuthSession.startAsync({
+        authUrl,
+      })) as AuthorizationResponse;
+
+      if (type === "success") {
+        const response = await fetch(
+          `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`
+        );
+        const userInfo = await response.json();
+
+        setUser({
+          id: userInfo.id,
+          email: userInfo.email,
+          name: userInfo.given_name,
+          photo: userInfo.picture,
+        });
+      }
+    } catch (error) {
+      throw new Error(error);
     }
+  }
+
   return (
-    <AuthContext.Provider value={{user}}>
-     {children}
+    <AuthContext.Provider value={{ user, signInWithGoogle }}>
+      {children}
     </AuthContext.Provider>
   );
 }
 
-function useAuth(){
-    const context = useContext(AuthContext)
-    return context
+function useAuth() {
+  const context = useContext(AuthContext);
+  return context;
 }
 
-export {AuthProvider, useAuth}
+export { AuthProvider, useAuth };
