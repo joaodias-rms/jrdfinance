29db061810ee4587a84972e73ec91e40be8922c8
joao <jvd.ramos@hotmail.com>
2022-02-17 13:52:30 -0400

    refatorado input para testes

--- src/components/Forms/Input/styles.ts
+++ src/components/Forms/Input/styles.ts
@@ -1,17 +1,26 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { TextInput } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
-export const Container = styled(TextInput)`
-    background-color: ${({theme})=> theme.colors.shape};
-    color: ${({theme})=> theme.colors.text_dark};
+interface Props {
+  active: boolean;
+}
 
-    width: 100%;
-    padding: 18px;
+export const Container = styled(TextInput)<Props>`
+  background-color: ${({ theme }) => theme.colors.shape};
+  color: ${({ theme }) => theme.colors.text_dark};
 
-    font-size: ${RFValue(14)}px;
-    font-family: ${({theme})=> theme.fonts.regular};
+  width: 100%;
+  padding: 18px;
 
-    border-radius: 5px;
-    margin-bottom: 8px;
-`
+  font-size: ${RFValue(14)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+
+  border-radius: 5px;
+  margin-bottom: 8px;
+
+  ${({ active, theme }) => active && css`
+    border-width: 3px;
+    border-color: ${theme.colors.attention}
+  `}
+`;
