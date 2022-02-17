9eae573e312d61a59700069fd56678f23e6995b9
joao <jvd.ramos@hotmail.com>
2022-02-17 17:09:54 -0400

    teste de falha de autenticação com google

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -3,12 +3,18 @@ import { mocked } from "jest-mock";
 import { AuthProvider, useAuth } from "./auth";
 import { startAsync } from "expo-auth-session";
 import fetchMock from "jest-fetch-mock";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 fetchMock.enableMocks();
 
 jest.mock("expo-auth-session");
 
 describe("Auth-Hook", () => {
+  beforeEach(async () => {
+    const userCollectionKey = "@jdrfinance:user";
+    await AsyncStorage.removeItem(userCollectionKey);
+  });
+
   it("should be able to sign in with a valid google account", async () => {
     const googleMocked = mocked(startAsync as any);
 
@@ -21,9 +27,9 @@ describe("Auth-Hook", () => {
 
     fetchMock.mockResponseOnce(
       JSON.stringify({
-        id: "any_email",
+        id:    "any_id",
         email: "jvd.ramos@hotmail.com",
-        name: "João Victor",
+        name:  "João Victor",
         photo: "any_photo.png",
       })
     );
@@ -34,7 +40,22 @@ describe("Auth-Hook", () => {
 
     await act(async () => await result.current.signInWithGoogle());
 
-    expect(result.current.user.email)
-    .toBe("jvd.ramos@hotmail.com");
+    expect(result.current.user.email).toBe("jvd.ramos@hotmail.com");
+  });
+
+  it("should not connect if cancel the google authentication", async () => {
+    const googleMocked = mocked(startAsync as any);
+
+    googleMocked.mockReturnValueOnce({
+      type: "cancel",
+    });
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    await act(async () => await result.current.signInWithGoogle());
+
+    expect(result.current.user).not.toHaveProperty("id");
   });
 });
